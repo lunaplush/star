83e458a784c95d9ad272d9b92a7caf5c45ad5cb0
lunaplush <lunaplus@mail.ru>
2018-10-03 11:06:15 +0300

    add data PS

---
+++ GAN.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+np.randomseed(15)
+
+
+
+#-----------------------GENERATOR----------------------------
+def get_generator(nois_dim, out_dim, hidden_dim = 100):
+    layers = [
+        nn.Linear(nois_dim,hidden_dim),
+        nn.LeakyReLU(),
+        nn.Linear(hidden_dim,hidden_dim)
+        nn.LeakyReLU()
+        nn.Linear(hidden_dim,out_dim)
+    ]
+    return nn.Sequential(*layers)
+
+#---------------------DISCRIMINATOR--------------------------
+def get_discriminator(in_dim, hidden_dim = 100):
+    layesr = [
+        nn.Linear(in_dim,hidden_dim),
+        nn.LeakyReLU(),
+        nn.Linear(hidden_dim,hidden_dim),
+        nn.LeakyReLU(),
+        nn.Linear(hidden_dim,hidden_dim),
+        nn.LeakyReLU(),
+        nn.Linear(hidden_dim,1),
+        nn.Sigmoid()
+    ]
+    return nn.Sequential(*layesr)
+#--------------------OPTIMIZATION---------------------------
+P_DATA_DIM = 2
+NOISE_DIM = 20
+generator =     get_generator(NOISE_DIM,out_dim = P_DATA_DIM)
+discriminator = get_discriminator(in_dim = P_DATA_DIM)
+
+lr = 0.001
+g_optimizer = optim.Adam(generator.parameters(),    lr = lr, betas = (0.5,0.999))
+d_optimizer = optim.Adam(discriminator.parameters(),lr = lr, betas = (0.5,0.999))
