0c702ee5d4313244306de72f51d5cb8bebb94465
lunaplush <lunaplus@mail.ru>
2018-10-05 15:38:07 +0300

    Outlier detection

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ import csv
 
 from matplotlib.colors import ListedColormap
 from sklearn import datasets, linear_model, metrics
+from sklearn.neighbors import LocalOutlierFactor
 
+%matplotlib inline
 
 np.random.seed(15)
-INPUT_DATA = 1 # 1 - Model data
+INPUT_DATA = 2 # 1 - Model data
                # 2 - PetroSpec
                
 #%% read data from file
@@ -50,13 +52,30 @@ if INPUT_DATA == 2:
 #%% model data
 if INPUT_DATA == 1:
     
-    [X,Y] = datasets.make_blobs(n_samples = 500, n_features = 2, centers = 5, cluster_std = [0.2,0.2,0.3,0.15,0.15], random_state = 57)
+    [X,Y] = datasets.make_blobs(n_samples = 500, n_features = 2, centers = 4, cluster_std = [0.2,0.2,0.3,0.15,0.15], random_state = 58)
     
 #%%
+plt.figure(1)
+plt.scatter(X[:,2],X[:,10], c = Y, cmap = plt.cm.Paired, s = 13)
 
+
+#%%
+
+clf = LocalOutlierFactor(n_neighbors = 20,contamination = 0.1 )
+y_pred = clf.fit_predict(X)
+X_scores = clf.negative_outlier_factor_
+plt.figure(2)
+
+plt.scatter(X[:,2],X[:,10], c = Y, cmap = plt.cm.Paired, s = 3)
+
+plt.scatter(X[y_pred == 1][:,2],X[y_pred == 1][:,10], c = Y[y_pred == 1], cmap = plt.cm.Paired, s = 33, marker = "*")
+
+#%%
+    
 #Logistic Regression
 
 multi_class = "multinomial"
 clf = linear_model.LogisticRegression(solver ="sag", penalty= "l2", max_iter= 1000,random_state= 15, multi_class= multi_class).fit(X,Y)
-
 print("training score : %.3f (%s)" % (clf.score(X, Y), multi_class))
+
+
